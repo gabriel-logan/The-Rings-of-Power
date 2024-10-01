bb8266f5b0cd3c02949e04c6bf5f8227313f4d90
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-10-01 17:19:20 -0300

    refactor: Remove length validation for newPassword in UpdateUserDto, add manual validation

--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,11 +1,10 @@
 import { ApiProperty, PartialType } from "@nestjs/swagger";
-import { IsString, Length } from "class-validator";
+import { IsString } from "class-validator";
 
 import { CreateUserDto } from "./create-user.dto";
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
-  @Length(4, 255)
   @ApiProperty()
   public newPassword?: string;
 }
