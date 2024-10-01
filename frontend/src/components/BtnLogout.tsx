0344e15a72e5528e1c0568843a7348b64ba512b6
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2024-10-01 17:05:51 -0300

    refactor: Update validation rules and error messages
    - Update validation rules for username and password in the login and register pages
    - Display more specific error messages for invalid username and password lengths
    - Dispatch logou via server action

--- frontend/src/components/BtnLogout.tsx
+++ frontend/src/components/BtnLogout.tsx
@@ -1,7 +1,6 @@
-"use client";
+import * as motion from "framer-motion/client";
 
-import { motion } from "framer-motion";
-import { signOut } from "next-auth/react";
+import { handleLogoutServer } from "@/actions";
 
 interface BtnLogoutProps {
   className?: string;
@@ -10,17 +9,15 @@ interface BtnLogoutProps {
 
 export default function BtnLogout({ className, children }: BtnLogoutProps) {
   return (
-    <motion.button
-      whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.95 }}
-      className={className}
-      onClick={async () => {
-        await signOut({
-          redirectTo: "/",
-        });
-      }}
-    >
-      {children ? children : "Logout"}
-    </motion.button>
+    <motion.form>
+      <motion.button
+        formAction={handleLogoutServer}
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        className={className}
+      >
+        {children ? children : "Logout"}
+      </motion.button>
+    </motion.form>
   );
 }
